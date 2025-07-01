932efca6c53f616db94953ab33caece604d3439a
Lucas Gonzatto <lucasgonzatto23@gmail.com>
2025-07-01 00:31:46 -0400

    Update t2exercicio4.py

--- t2exercicio4.py
+++ t2exercicio4.py
@@ -1,10 +1,8 @@
 a = 80000
 b = 200000
 anos = 0
-
 while a < b:
     a += a * 0.03
     b += b * 0.015
     anos += 1
-
 print("Levará", anos, "anos para a população de A passar ou igualar a de B.")
